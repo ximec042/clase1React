90ad9dc8b0de4fae94f099af1263017584a893f8
ximena <ximec042@gmail.com>
2023-09-19 03:26:16 -0300

    Clase 1 react

---
+++ src/component/Footer/index.js
@@ -0,0 +1,58 @@
+const Footer = () => {
+  return (
+    <>
+      <footer class="pie">
+        <div class="grupo1">
+          <div class="box">
+            <figure>
+              <a href="#">
+                <img src="./imagenes/footer.png" alt="logo footer" />
+              </a>
+            </figure>
+          </div>
+
+          <div
+            class="box"
+            style={{
+              marginTop: 50,
+            }}
+          >
+            <h2>SOBRE NOSOTROS</h2>
+            <ul>
+              <li>Quienes somos</li>
+              <li>Ubicación</li>
+              <li>Acerca de</li>
+              <li>Trabaja con nosotros</li>
+            </ul>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, autem!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Necessitatibus, autem!
+            </p>
+          </div>
+
+          <div class="box" style={{ marginTop: 50 }}>
+            <h2>SIGUENOS</h2>
+            <div class="red-social">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-instagram"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-youtube"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="grupo2">
+          <small>
+            Copyright <i class="fa fa-copyright"></i> 2023 <b>ECO</b>
+          </small>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/component/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <header>
+        <div id="nombre">
+          <img id="imagen" src="./images/logo.png" alt="logo" width="150px" />
+        </div>
+  
+        <nav id="links">
+          <a href="empresa.html" target="_blank">EMPRESA</a>
+          <a href="contacto.html" target="_blank">CONTACTO</a>
+          <a href="registro.html" target="_blank">REGISTRO</a>
+        </nav>
+      </header>
+    );
+}
+
+export default Header;
